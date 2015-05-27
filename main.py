d3f4c65a0f503d5e65597ae761a484f01ecafc37
whiker <whiker@163.com>
2015-05-27 11:05:45 +0800

    filter guassian

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ from numpy import *
 import matplotlib.pyplot as plt
 import Image
 import pca
+from scipy.ndimage import filters
 
 
 if __name__ == '__main__':
-	x = [[0,1],[1,0],[1,2],[2,1],[2,3],[3,2],[3,4],[4,3]];
-	x = array(x);
-	V,S,meanX = pca.PCA(x);
-	print V;
-	print S;
-	print meanX;
+	img = array(Image.open('img/lion_2.jpg').convert('L'));
+	dst = zeros(img.shape);
+	filters.gaussian_filter(img, (3,3), (0,1), dst);
+	dstMin = dst.min();
+	dstMax = dst.max();
+	dst = (dst - dstMin) * 255 / (dstMax - dstMin);
+	Image.fromarray(uint8(dst)).save('img/lion_2_1.jpg');
